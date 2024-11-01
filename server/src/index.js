ca69c19d7997c55bbd139d6896de9a4700567344
RAMON DE HOLANDA NASCIMENTO <rholanda@labcetec.net>
2024-11-01 16:55:22 -0300

    Finalizando CRUD READ UPDATE e DELETE

--- server/src/index.js
+++ server/src/index.js
@@ -1,11 +1,12 @@
 import express from 'express';
 import fileUpload from 'express-fileupload';
-import {criarImagem, mostrarImagem} from './controllers/ImagemController.js';
+import {criarImagem, deletandoImagem, editarImagem, mostrarImagem, mostrarImagens} from './controllers/ImagemController.js';
 
 const app = express();
 const porta = 5000;
 
 app.use(fileUpload());
+app.use(express.json());
 
 app.get('/',(req,res)=>{
     res.send('API Funcionando!')
@@ -14,7 +15,10 @@ app.get('/',(req,res)=>{
 app.get('/public/:nomeImg',mostrarImagem);
 //CRUD Imagem
 app.post('/imagem',criarImagem);
-
+app.get('/imagem',mostrarImagens);
+//app.get('/imagem/:id_imagem',mostrarUmaImagem);
+app.put('/imagem/:id_imagem',editarImagem);
+app.delete('/imagem/:id_imagem',deletandoImagem);
 
 app.listen(porta, ()=>{
     console.log(`API Rodando na porta ${porta}`)
